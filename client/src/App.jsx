9239754eb5ad5128de076ea56bece49234a7a40d
Cheon Beom Ik <kgkb0813@gmail.com>
2024-11-08 18:48:43 +0900

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
-// import Header from './components/Header';
+import Header from './components/Header';
 // import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
