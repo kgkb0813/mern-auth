af4b0442eae6828799e19c3d22e4628d2675da37
Cheon Beom Ik <kgkb0813@gmail.com>
2024-11-11 12:07:33 +0900

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
+router.post('/signin', signin);
 router.post('/signup', signup);
 
 export default router;
@@ -9,7 +10,6 @@ export default router;
 // import { signin, signup, google, signout } from '../controllers/auth.controller.js';
 
 
-// router.post('/signin', signin);
 // router.post('/google', google);
 // router.get('/signout', signout);
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -34,10 +34,10 @@ export default function SignUp() {
     } catch (error) {
       setLoading(false);
       setError(true);
+      console.log(error.message)
     }
   };
 
-  console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
