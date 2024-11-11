b57c51944521354b5ba26fb9f011fa8e41aa4f36
Cheon Beom Ik <kgkb0813@gmail.com>
2024-11-11 13:59:41 +0900

    complete the sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,5 @@
-
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
-
-export default SignIn
-
-// import { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 // import {
 //   signInStart,
 //   signInSuccess,
@@ -17,73 +8,85 @@ export default SignIn
 // import { useDispatch, useSelector } from 'react-redux';
 // import OAuth from '../components/OAuth';
 
-// export default function SignIn() {
-//   const [formData, setFormData] = useState({});
-//   const { loading, error } = useSelector((state) => state.user);
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  // const { loading, error } = useSelector((state) => state.user);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
-//   const handleChange = (e) => {
-//     setFormData({ ...formData, [e.target.id]: e.target.value });
-//   };
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       dispatch(signInStart());
-//       const res = await fetch('/api/auth/signin', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(formData),
-//       });
-//       const data = await res.json();
-//       if (data.success === false) {
-//         dispatch(signInFailure(data));
-//         return;
-//       }
-//       dispatch(signInSuccess(data));
-//       navigate('/');
-//     } catch (error) {
-//       dispatch(signInFailure(error));
-//     }
-//   };
-//   return (
-//     <div className='p-3 max-w-lg mx-auto'>
-//       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-//       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-//         <input
-//           type='email'
-//           placeholder='Email'
-//           id='email'
-//           className='bg-slate-100 p-3 rounded-lg'
-//           onChange={handleChange}
-//         />
-//         <input
-//           type='password'
-//           placeholder='Password'
-//           id='password'
-//           className='bg-slate-100 p-3 rounded-lg'
-//           onChange={handleChange}
-//         />
-//         <button
-//           disabled={loading}
-//           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-//         >
-//           {loading ? 'Loading...' : 'Sign In'}
-//         </button>
-//         <OAuth />
-//       </form>
-//       <div className='flex gap-2 mt-5'>
-//         <p>Dont Have an account?</p>
-//         <Link to='/sign-up'>
-//           <span className='text-blue-500'>Sign up</span>
-//         </Link>
-//       </div>
-//       <p className='text-red-700 mt-5'>
-//         {error ? error.message || 'Something went wrong!' : ''}
-//       </p>
-//     </div>
-//   );
-// }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // dispatch(signInStart());
+      setError(false);
+      setError(false);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        // dispatch(signInFailure(data));\
+        setError(true);
+        return;
+      }
+      // dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+      // dispatch(signInFailure(error));
+    }
+  };
+  
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <button
+          // disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        {/* <OAuth /> */}
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-5'>
+        {error ? error.message || 'Something went wrong!' : ''}
+      </p> 
+
+    </div>
+  );
+}
